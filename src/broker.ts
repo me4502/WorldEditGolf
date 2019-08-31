a9c37c67fd02ac06c40e030bb743337a3fad3413
Matthew Miller <mnmiller1@me.com>
2019-08-31 17:23:41 +1000

    feat: add broker api

---
+++ src/broker.ts
@@ -0,0 +1,38 @@
+type BrokerStatus = 'queued' | 'errored' | 'failed' | 'passed' | 'running';
+
+const BROKER_API_HOSTNAME = 'localhost:4653';
+
+interface QueueRequest {
+  input: string;
+  test: string;
+  script: string;
+}
+
+interface QueueResponse {
+  taskId: string;
+}
+
+interface PollResponse {
+  status: BrokerStatus;
+  positionInQueue?: number;
+  log?: string;
+  reason?: string;
+  result?: string;
+}
+
+export async function queueTask(request: QueueRequest): Promise<QueueResponse> {
+  return (await fetch(`${BROKER_API_HOSTNAME}/queue`, {
+    method: 'post',
+    body: JSON.stringify(request)
+  })).json();
+}
+
+export async function clearTask(taskId: string): Promise<void> {
+  await fetch(`${BROKER_API_HOSTNAME}/clear/${taskId}`);
+}
+
+export async function pollBroker(taskId: string): Promise<PollResponse> {
+  return (await fetch(`${BROKER_API_HOSTNAME}/poll/${taskId}`, {
+    method: 'post'
+  })).json();
+}
