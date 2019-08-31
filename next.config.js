6795e3442e400df7d5fa08a5babbcdea85b0c354
Bennett Hardwick <me@bennetthardwick.com>
2019-08-31 21:52:11 +1000

    Add env

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ module.exports = {
     assetsPrefix: ASSETS_PREFIX,
     env: {
         STATIC_PREFIX: prod ? ASSETS_PREFIX : '',
+        CLIENT_ID: process.env.CLIENT_ID,
+        CLIENT_SECRET: process.env.CLIENT_SECRET,
+        JWT_SECRET: process.env.JWT_SECRET
         // GA_TRACKING_ID: 'UA-139849956-4'
     }
 };
